21d7bd692eca3245310c44b40caf276ce079464d
EthanKettle <97110734+EthanKettle@users.noreply.github.com>
2022-07-22 09:52:52 -0600

    unit 3

--- hr-timesheet/src/app/app.module.ts
+++ hr-timesheet/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { MaterialModule } from './modules/material.module';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { AnalyticsTableComponent } from './components/analytics-table/analytics-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
