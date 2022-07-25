5a623661d7073ed9b2a2161af9022be9d0af7746
EthanKettle <97110734+EthanKettle@users.noreply.github.com>
2022-07-25 11:32:48 -0600

    blank

--- hr-timesheet/src/app/modules/app-routing.module.ts
+++ hr-timesheet/src/app/modules/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentsComponent } from '../components/departments/departments.component';
+import { TimesheetComponent } from '../components/timesheet/timesheet.component';
+import { AnalyticsComponent  } from '../components/analytics/analytics.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'departments', pathMatch: 'full' },
+  { path: 'departments', component: DepartmentsComponent },
+  { path: 'timesheet', component: TimesheetComponent },
+  { path: 'analytics', component: AnalyticsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
